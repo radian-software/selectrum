d63a318984a0e76571f32672ae8502512c638ce1
Radon Rosborough <radon.neon@gmail.com>
2020-03-23 17:22:29 -0600

    Add commentary to selectrum.el

--- selectrum.el
+++ selectrum.el
@@ -11,6 +11,19 @@
 
 ;;; Commentary:
 
+;; Selectrum is a better solution for incremental narrowing in Emacs,
+;; replacing Helm, Ivy, and IDO. Its design philosophy is based on
+;; choosing the right abstractions and prioritizing consistency and
+;; predictability over special-cased improvements for particular
+;; cases. As such, Selectrum follows existing Emacs conventions where
+;; they exist and are reasonable, and it declines to implement
+;; features which have marginal benefit compared to the additional
+;; complexity of a new interface.
+
+;; Getting started: Selectrum provides a global minor mode,
+;; `selectrum-mode', which enhances `completing-read' and all related
+;; functions automatically without the need for further configuration.
+
 ;; Please see https://github.com/raxod502/selectrum for more
 ;; information.
 
