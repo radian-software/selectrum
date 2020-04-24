29e4adbfe8b0a0915b81dc433325f51ba72bd534
Clemens Radermacher <clemera@posteo.net>
2020-04-24 10:35:29 +0200

    Improve selectrum-read-library-name
    Don't show `.el` suffixes and don't insert whole path on `TAB` as reported by
    [oantolin](https://www.reddit.com/user/oantolin)
    [here](https://www.reddit.com/r/emacs/comments/g6ocid/orderless_a_completion_style_that_matches/foegie3/)

--- selectrum.el
+++ selectrum.el
@@ -1366,15 +1366,14 @@ shadows correctly."
                  (let ((candidate-paths
                         (mapcar (lambda (path)
                                   (propertize
-                                   (file-name-base path)
+                                   (file-name-base
+                                    (file-name-sans-extension path))
                                    'selectrum-candidate-display-prefix
                                    (file-name-directory
                                     (file-name-sans-extension
                                      (selectrum--trailing-components
                                       num-components path)))
-                                   'fixedcase 'literal
-                                   'selectrum-candidate-full
-                                   path))
+                                   'fixedcase 'literal))
                                 paths)))
                    (setq lst (nconc candidate-paths lst)))
                  (cl-return)))
