1aed1c9f98ab918f95a737ecba7fe07d34d57476
Clemens Radermacher <clemera@posteo.net>
2021-01-10 16:16:45 +0100

    Improve prompt selection for history commands (#346)

--- selectrum.el
+++ selectrum.el
@@ -821,7 +821,8 @@ the update."
                                   (minibuffer-contents)))
                       (and (not (= (minibuffer-prompt-end) (point-max)))
                            (memq this-command '(next-history-element
-                                                previous-history-element))))
+                                                previous-history-element))
+                           (not selectrum--match-required-p)))
                   -1)
                  (selectrum--move-default-candidate-p
                   0)
