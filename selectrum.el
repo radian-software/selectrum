f6d8b5cc79e9d99efe3a8211422c2d0ebeb41b12
Clemens Radermacher <clemera@posteo.net>
2021-01-13 13:50:52 +0100

    No prompt selection when back to start position

--- selectrum.el
+++ selectrum.el
@@ -837,10 +837,8 @@ the update."
                            (equal selectrum--default-candidate
                                   (minibuffer-contents)))
                       (and (not (= (minibuffer-prompt-end) (point-max)))
-                           (or (memq this-command '(next-history-element
-                                                    previous-history-element))
-                               (and minibuffer-history-position
-                                    (not (zerop minibuffer-history-position))))
+                           (and minibuffer-history-position
+                                (not (zerop minibuffer-history-position)))
                            (or (not selectrum--match-required-p)
                                (selectrum--at-existing-prompt-path-p))))
                   -1)
