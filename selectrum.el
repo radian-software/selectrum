6a63e08e5fee060c4704d13d08fde1acfc547c6c
Clemens Radermacher <clemera@posteo.net>
2021-02-11 23:12:03 +0100

    Include line spacing when using fixed vertical window height (#432)

--- selectrum.el
+++ selectrum.el
@@ -1292,24 +1292,24 @@ window is supposed to be shown vertically."
                    (and (window-at-side-p window 'bottom)
                         (not (window-at-side-p window 'top))))
            (set-window-text-height window 1)))
-        ((and (or (bound-and-true-p selectrum-fix-minibuffer-height)
-                  selectrum-fix-vertical-window-height)
-              vertical)
+        ((and vertical selectrum-fix-vertical-window-height)
          (let* ((max (selectrum--max-window-height))
-                (height (if selectrum-display-action
-                            max
-                          ;; Add one for prompt.
-                          (1+ max))))
-           (set-window-text-height window height)))
+                (lines (if selectrum-display-action
+                           max
+                         ;; Add one for prompt.
+                         (1+ max)))
+                ;; Include possible line spacing.
+                (height (* lines (line-pixel-height))))
+           (selectrum--set-window-height window height)))
         (t
          (when-let ((expand (selectrum--expand-window-for-content-p window)))
            (cond (selectrum-display-action
-                  (selectrum--update-display-window-height window))
+                  (selectrum--fit-window-to-buffer window))
                  (t
-                  (selectrum--update-minibuffer-height window)))))))
+                  (selectrum--set-window-height window)))))))
 
-(defun selectrum--update-display-window-height (window)
-  "Update window height of WINDOW.
+(defun selectrum--fit-window-to-buffer (window)
+  "Fit window height to its buffer contents.
 Also works for frames if WINDOW is the root window of its frame."
   (let ((window-resize-pixelwise t)
         (window-size-fixed nil)
@@ -1317,10 +1317,11 @@ Also works for frames if WINDOW is the root window of its frame."
         (fit-window-to-buffer-horizontally nil))
     (fit-window-to-buffer window nil 1)))
 
-(defun selectrum--update-minibuffer-height (window)
-  "Update window height of minibuffer WINDOW.
-WINDOW will be updated to fit its content vertically."
-  (let ((dheight (cdr (window-text-pixel-size window)))
+(defun selectrum--set-window-height (window &optional height)
+  "Set window height of WINDOW to HEIGHT pixel.
+If HEIGHT is not given WINDOW will be updated to fit its content
+vertically."
+  (let ((dheight (or height (cdr (window-text-pixel-size window))))
         (wheight (window-pixel-height window))
         (window-resize-pixelwise t))
     (window-resize
