4f47170bd0f07412f86c61ab78045243773e4f33
Clemens Radermacher <clemera@posteo.net>
2021-02-17 19:02:42 +0100

    Add bug-reference setup

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,4 @@
 ((nil . ((compile-command . "make lint")
+         (bug-reference-bug-regexp . "#\\(?2:[0-9]+\\)")
+	     (bug-reference-url-format . "https://github.com/raxod502/selectrum/issues/%s")
          (fill-column . 70))))
