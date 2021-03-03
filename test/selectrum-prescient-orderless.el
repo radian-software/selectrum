ea22659491eda3c46ffb2471c72c31764913c2a5
Clemens Radermacher <clemera@posteo.net>
2021-03-03 17:59:07 +0100

    Add selectrum-prescient-orderless

---
+++ test/selectrum-prescient-orderless.el
@@ -0,0 +1,18 @@
+;; -*- lexical-binding: t -*-
+
+;; Setup completion style which is responsible for candidate filtering
+(setq completion-styles '(orderless))
+
+;; Enable Selectrum, Prescient and Marginalia
+(selectrum-mode)
+;; Disable filtering as orderless is used for that
+(setq selectrum-prescient-enable-filtering nil)
+(selectrum-prescient-mode)
+(marginalia-mode)
+
+;; Use completing-read prompter and set binding for Embark context menu
+(setq embark-prompter #'embark-completing-read-prompter)
+(global-set-key (kbd "M-o") #'embark-act)
+
+;; Keybindings for Consult
+(global-set-key (kbd "C-x b") #'consult-buffer)
